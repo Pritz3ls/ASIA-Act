1af678d3330c96b32f9e5c366f6b9527813834f3
Pritzels <princefinn14@gmail.com>
2025-05-04 14:23:33 +0800

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,29 @@
-// That's a lot of component i must say, wished i could restructure this to be more organized
-import Navbar from './components/Navbar.jsx'
+import './App.css'
+import Nav from './components/Nav.jsx'
 import Hero from './components/Hero.jsx'
-import Counter from './components/Counter.jsx'
-import Collaborate from './components/Collaborate.jsx'
-import Users from './components/Users.jsx'
-import HowWork from './components/HowWork.jsx'
-import Benefit from './components/Benefit.jsx'
-import Reviews from './components/Reviews.jsx'
-import Pricing from './components/Pricing.jsx'
-import FAQ from './components/FAQ.jsx'
-import Contact from './components/Contact.jsx'
-import Footer from './components/Footer.jsx'
-import BackStartButton from './components/BackStartButton.jsx'
-
-/*
-  Refactored and Reviewed by Prince Marco R. Guerrero :D :D :D 
-*/
+import Team from './components/Team.jsx'
+import Collab from './components/Collab.jsx'
+import Footer from './components/footer/Footer.jsx'
 
 function App() {
+
+
   return (
     <> 
+    <main class="main" id="top" scroll-to-top="scroll-to-top">
+      <div class="content">
+       
+           <Nav/>
 
-      <main className="main" id="top" scroll-to-top="scroll-to-top">
-        <div className="content">
-          <Navbar/>
-          <div data-bs-target="#navbar" data-bs-spy="scroll" tabIndex="0">
-              {/* <Other components/> */}
-              {/* Ohh i get it, it's like C# OOP but on React, HOLY Crap Lois! 
-              I remember the time I'm in ReactJS script and start reacting on everybody */}
-              <Hero/>
-              <Counter/>
-              <Users/>
-              <Collaborate/>
-              <HowWork/>
-              <Benefit/>
-              <Reviews/>
-              <Pricing/>
-              <FAQ/>
-              <Contact/>
-          </div>
-          <BackStartButton/>
+       <div data-bs-target="#navbar" data-bs-spy="scroll" tabindex="0">
+          <Hero/>
+          <Team/>
+          <Collab/>
           <Footer/>
-        </div>
-      </main>
+      </div>
+
+     </div>
+    </main>
 
     </>
   )
